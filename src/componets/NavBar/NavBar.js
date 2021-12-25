f6f62ddb55d94c9ba5565b63c67c9fb016799e7f
Len <justlenard.justme@gmail.com>
2021-12-25 19:03:15 +0200

    trying to load sass. Doesn't work

---
+++ src/componets/NavBar/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const NavBar = () => {
+	return (
+		<div className="navbar">
+			<h1>Productsl list</h1>
+			<div class="add-delete">
+				<button>add</button>
+				<button>delete</button>
+			</div>
+		</div>
+	);
+};
+
+export default NavBar;
