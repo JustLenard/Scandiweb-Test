7e1eb8f606bc675be0572cc90a92cdf865b3f35e
Len <justlenard.justme@gmail.com>
2021-12-26 18:09:59 +0200

    card

--- src/componets/App.js
+++ src/componets/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import NavBar from './NavBar/NavBar';
+import Card from './Card/Card';
 import './App.sass';
 
 const App = () => {
 	return (
 		<div class="main-container">
 			<NavBar />
+			<Card />
 		</div>
 	);
 };
---
+++ src/componets/Card/Card.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Card.sass';
+
+const Card = props => {
+	return (
+		<div className={'card'}>
+			<input type={'radio'}></input>
+			<div>1</div>
+			<div>2</div>
+			<div>3</div>
+			<div>4</div>
+		</div>
+	);
+};
+
+export default Card;
