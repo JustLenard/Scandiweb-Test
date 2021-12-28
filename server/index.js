5c5f4139ea68946c716f9f601693585208d6cb4f
Len <justlenard.justme@gmail.com>
2021-12-28 16:29:15 +0200

    yea, it will take a wihle to get it

--- server/index.js
+++ server/index.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const app = express();
+const mysql = require('mysql');
+
+const db = mysql.createPool({
+	host: 'localhost',
+	user: 'root',
+	password: 'password',
+	database: 'ScandiwebDB',
+});
 
 app.get('/', (req, res) => {
-	res.send('Hello World!!!!');
+	res.send('Hello World!');
+	const sqlInsert =
+		'INSERT INTO product (name-roduct, price-product) VALUES (?,?)';
+	db.query(sqlInsert, [nameProduct, priceProduct], (err, result) => {});
 });
 
 app.listen(3001, () => {
