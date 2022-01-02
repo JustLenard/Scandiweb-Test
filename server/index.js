801cb4fd7c0d2d0655b69472b40f6019695f0cd9
Len <justlenard.justme@gmail.com>
2022-01-02 17:59:44 +0200

    Some progress. Can connect now to the Db

--- server/index.js
+++ server/index.js
@@ -1,21 +1,47 @@
 const express = require('express');
+var connection = require('./database.js');
 const app = express();
-const mysql = require('mysql');
 
-const db = mysql.createPool({
-	host: 'localhost',
-	user: 'root',
-	password: 'password',
-	database: 'ScandiwebDB',
-});
+// const db = mysql.createPool({
+// 	host: 'localhost',
+// 	user: 'admin',
+// 	password: 'password',
+// 	database: 'ScandiwebDB',
+// });
 
+// const db = mysql.createConnection({
+// 	host: 'localhost',
+// 	user: 'admin',
+// 	password: 'password',
+// 	database: 'ScandiwebDB',
+// });
+
+// app.get('/', (req, res) => {
+// 	let sql = 'SELECT * FROM ScandiwebDB.new_table;';
+// 	connection.query(sql, (err, results) => {
+// 		if (err) throw err;
+// 		res.send(results);
+// 	});
+// });
 app.get('/', (req, res) => {
-	res.send('Hello World!');
 	const sqlInsert =
-		'INSERT INTO product (name-roduct, price-product) VALUES (?,?)';
-	db.query(sqlInsert, [nameProduct, priceProduct], (err, result) => {});
+		'INSERT INTO new_table (name_product, price_product) VALUES (stick, 3)';
+	// db.query(sqlInsert, [nameProduct, priceProduct], (err, result) => {});
+	connection.query(sqlInsert, (err, results) => {
+		if (err) throw err;
+		res.send(results);
+	});
 });
 
 app.listen(3001, () => {
-	console.log('seems to be working');
+	console.log('seems to be working!!');
+	connection.connect(err => {
+		if (err) throw err;
+		console.log('Connected to DB');
+	});
 });
+
+// db.connect(function (err) {
+// 	if (err) throw err;
+// 	console.log('Connected!');
+// });
--- test.session.sql
+++
@@ -1,6 +0,0 @@
--- @block
-
-CREATE TABLE Users(
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    productName VARCHAR(255) NOT NULL ,
-);
