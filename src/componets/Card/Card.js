0b30be10102bde261cec2a5424bc453415378118
Len <justlenard.justme@gmail.com>
2022-01-09 19:54:05 +0200

    Trying to deal with the checkboxes. IT's trickier than I thought.

--- src/componets/Card/Card.js
+++ src/componets/Card/Card.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './Card.sass';
 
 const Card = props => {
-	console.log(props.Size);
 	const typeDiferences = {
 		Book: <div>Weight: {props.Size} KG</div>,
 		DVD: <div>Size: {props.Size} MB</div>,
@@ -13,9 +12,22 @@ const Card = props => {
 		),
 	};
 
+	// const selectDelete = e => {
+	// 	if (e.target.checked === true) {
+	// 		const newAr = props.dbProducts.filter(
+	// 			product =>
+	// 				// product.Sku;
+	// 				product.Sku !== props.Sku
+	// 			// console.log(product.Sku)
+	// 		);
+	// 		// console.log(props.dbProducts);
+	// 		console.log(newAr);
+	// 	}
+	// };
+
 	return (
 		<div className={'card'}>
-			<input type={'checkBox'}></input>
+			<input type={'checkBox'} onClick={}></input>
 			<div>Sku: {props.Sku}</div>
 			<div>Name: {props.Name}</div>
 			<div>Price: {props.Price} $</div>
--- src/componets/Pages/Home.js
+++ src/componets/Pages/Home.js
@@ -9,18 +9,36 @@ const Home = props => {
 	const [dbProducts, setDbProducts] = useState([]);
 	useEffect(() => {
 		Axios.get('http://localhost:3001/api/get').then(response => {
+			response.data.map(inside => {
+				inside.checked = false;
+			});
+			// console.log(response.data);
 			setDbProducts(response.data);
 		});
 	}, []);
-
-	console.log(dbProducts);
-	console.log(dbProducts['Name']);
-	console.log(
-		dbProducts.map(product => {
-			console.log(product['Name']);
-		})
-	);
-
+	// console.log(dbProducts);
+	const [deleteList, setDeleteList] = useState(dbProducts);
+	// useEffect(() => {
+	// 	console.log('weird');
+	// 	const f = dbProducts;
+	// 	console.log(f);
+	// 	setDeleteList(dbProducts);
+	// }, []);
+	// deleteList = dbProducts;
+	// console.log(deleteList);
+	// console.log(dbProducts);
+	const handleCheck = () => {
+		// dbProducts[0].checked = !dbProducts[0].checked;
+		// console.log(dbProducts[0].checked);
+		// console.log(dbProducts);
+		// dbProducts[idProduct]
+	};
+	// console.log(dbProducts['Name']);
+	// console.log(
+	// 	dbProducts.map(product => {
+	// 		console.log(product['Name']);
+	// 	})
+	// );
 	return (
 		<div className="main-container">
 			<NavBar
@@ -31,6 +49,7 @@ const Home = props => {
 			/>
 			<div className={'card-container'}>
 				{dbProducts.map(dbProduct => {
+					// console.log(dbProducts);
 					return (
 						<Card
 							Price={dbProduct['Price']}
@@ -42,6 +61,13 @@ const Home = props => {
 							Type={dbProduct['Type']}
 							Weight={dbProduct['Weight']}
 							Width={dbProduct['Width']}
+							Checked={dbProduct['Checked']}
+							key={dbProduct['Sku']}
+							idProduct={dbProduct['IdProduct'] - 1}
+							dbProducts={dbProducts}
+							setDeleteList={setDeleteList}
+							deleteList={deleteList}
+							handleCheck={handleCheck}
 						/>
 					);
 				})}
