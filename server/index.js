3afbbd5f9f6dc7a4d0db1adc048006407344709a
Len <justlenard.justme@gmail.com>
2022-01-15 14:23:20 +0200

    n3

--- server/index.js
+++ server/index.js
@@ -9,14 +9,15 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('https://scandiwebtest.herokuapp.com/api/get', (req, res) => {
+app.get('/api/get', (req, res) => {
+	console.log('Hello');
 	const sqlSelect = `SELECT * FROM sql11465741.Products;`;
 	connection.query(sqlSelect, (err, result) => {
 		res.send(result);
 	});
 });
 
-app.delete('https://scandiwebtest.herokuapp.com/api/delete', (req, res) => {
+app.delete('/api/delete', (req, res) => {
 	const toDelete = req.body;
 	const sqlDelete = `DELETE FROM Products WHERE idProduct IN (${toDelete.join()})`;
 	connection.query(sqlDelete, (err, result) => {
@@ -24,7 +25,7 @@ app.delete('https://scandiwebtest.herokuapp.com/api/delete', (req, res) => {
 	});
 });
 
-app.post('https://scandiwebtest.herokuapp.com/api/insert', (req, res) => {
+app.post('/api/insert', (req, res) => {
 	const FrontEndObj = req.body;
 	const sqlInsert =
 		'INSERT INTO Products (Sku, Name, Price, Type, Size, Weight, Height, Length, Width) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)';
